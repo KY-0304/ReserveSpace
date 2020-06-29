52ef6820edec0708a3e25fb38988912a57246335
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-06-29 21:44:25 +0900

    full_titleヘルパーを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "iSpace"
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} - #{base_title}"
+    end
+  end
 end
