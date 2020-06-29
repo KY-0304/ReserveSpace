b42d6bb1ee269b9e61ff4120d3312b82739c05d2
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-06-29 13:46:26 +0900

    spring-commands-rspec導入

--- spec/support/config/factory_bot.rb
+++ spec/support/config/factory_bot.rb
@@ -1,4 +1,9 @@
 RSpec.configure do |config|
   # FactoryBotのメソッドを省略して使用できるようにする
   config.include FactoryBot::Syntax::Methods
+
+  # springが原因でfactoryが正しく読み込まれないことを防ぐ
+  config.before :all do
+    FactoryBot.reload
+  end
 end
