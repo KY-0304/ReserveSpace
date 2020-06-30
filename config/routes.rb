f656bc0233afcf0a1592850a5ff3f7d4dabb7414
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-06-30 22:07:34 +0900

    ownerログイン・ログアウトの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :owners
+  devise_for :owners, controllers: {
+    registrations: 'owners/registrations',
+    sessions: 'owners/sessions',
+  }
   root 'static_pages#home'
 end
