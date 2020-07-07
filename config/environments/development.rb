a9e6094d02db9d6e2cd386720eaf7b404167585e
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-07-08 03:33:53 +0900

    不要なコメントを削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # デフォルトのURLオプションを設定（deviseの設定：https://github.com/heartcombo/devise）
+  # デフォルトのURLオプションを設定
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # 送信したメールをletter_opener_webで見れるように設定
   config.action_mailer.delivery_method = :letter_opener_web
