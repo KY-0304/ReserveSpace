e9b7b69112a1e7b4bcaa174f61b25ed6aa6054eb
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-07-22 15:20:01 +0900

    Owners::Reservations#indexのビュー作成

--- app/controllers/owners/reservations_controller.rb
+++ app/controllers/owners/reservations_controller.rb
@@ -2,6 +2,7 @@ class Owners::ReservationsController < ApplicationController
   before_action :authenticate_owner!
 
   def index
+    @rooms = current_owner.rooms.includes(reservations: :user)
   end
 
   def show
