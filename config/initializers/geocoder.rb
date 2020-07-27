6857ec29828978fb51cdf669ae8a14f0baa70b87
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-07-27 20:17:33 +0900

    geocoderの検索結果をtmp/geocoderに30日間キャッシュする設定を追加

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,14 +3,14 @@ Geocoder.configure(
   timeout: 10,                 # geocoding service timeout (secs)
   lookup: :google,         # name of geocoding service (symbol)
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
-  # language: :en,              # ISO-639 language code
+  language: :ja,              # ISO-639 language code
   use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
   api_key: ENV['GOOGLE_MAPS_APIKEY'],               # API key for geocoding service
-  # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
-  # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
-
+  # tmp/geocoderに30日間検索結果をキャッシュする設定
+  cache: ActiveSupport::Cache::FileStore.new([Rails.root, 'tmp', 'geocoder'].join('/'), { expires_in: 30.days }),
+  cache_prefix: 'geocoder:',
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
   # supports SocketError and Timeout::Error
