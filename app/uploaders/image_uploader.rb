ecf425a3db7a52f90a00f3445f9e2c2c4eb616db
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-07-27 20:06:55 +0900

    imageuploaderにファイルサイズ制限追加、localeにcarrierwave.ja.ymlを追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,17 +1,21 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
+  # space詳細ページ用のサイズ
   process resize_to_fill: [700, 500]
 
+  # サムネイル用のサイズ
   version :thumb do
     process resize_to_fill: [200, 200]
   end
 
+  # アップロード時のサムネイル
   version :small_thumb, from_version: :thumb do
     process resize_to_fill: [50, 50]
   end
 
   storage :file
 
+  # テスト環境では独自のパスで保存する設定
   def store_dir
     if Rails.env.test?
       "uploads_#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -20,10 +24,17 @@ class ImageUploader < CarrierWave::Uploader::Base
     end
   end
 
+  # 許容するファイル拡張子
   def extension_whitelist
     %w(jpg jpeg png)
   end
 
+  # ファイルサイズを1KB〜5MBに制限
+  def size_range
+    1.kilobytes..5.megabytes
+  end
+
+  # spaceのimageがnilの時、表示する画像
   def default_url(*args)
     "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
