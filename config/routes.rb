e0f06a18f4b72a32a2dba56954e8c434b4072264
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-08-14 16:49:14 +0900

    ApplicationControllerにrescue_fromで例外補足するよう実装

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,6 @@ Rails.application.routes.draw do
   end
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+  match "*path" => "application#error404", via: :all
 end
