025bbe6ddf20757c9d850322532e1aad70354b29
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-09-07 17:44:38 +0900

    ownerモデルリファクタ

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,12 +1,12 @@
 class Owner < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   after_destroy :check_all_space_destroyed
+
   has_many :spaces, dependent: :destroy
 
-  validates :company_name, length: { maximum: 140 }, presence: true, uniqueness: true
-  validates :email,        length: { maximum: 255 }
+  validates :company_name, length: { maximum: MAX_COMPANY_NAME_LENGTH }, presence: true, uniqueness: true
+  validates :email,        length: { maximum: MAX_EMAIL_LENGTH }
   validates :agreement, acceptance: true
 
   def self.guest
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -9,3 +9,7 @@ MINIMUM_UNIT_ROOM_PRICE = 100
 CHECK_MINUTES = %w(00 15 30 45)
 
 GOOGLEMAP_API_JS_URL = "https://maps.googleapis.com/maps/api/js?key=#{ENV['GOOGLE_MAPS_APIKEY']}&callback=initMap"
+
+MAX_COMPANY_NAME_LENGTH = 140
+
+MAX_EMAIL_LENGTH = 255
