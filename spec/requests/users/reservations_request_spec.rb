6c15a62f029b087526ade42aee27fc1cc78ce61f
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-09-11 02:33:15 +0900

    Users::Reservations#create, destroyの処理をトランザクションに変更、それに伴いテスト変更

--- spec/requests/users/reservations_request_spec.rb
+++ spec/requests/users/reservations_request_spec.rb
@@ -133,19 +133,39 @@ RSpec.describe "Users::Reservations", type: :request do
       context "パラメータが不正な場合" do
         let(:invalid_params) { { space_id: space.id, start_time: "", end_time: "" } }
 
-        it "ステータスコード200を返す" do
+        it "ステータスコード500を返す" do
           post users_reservations_path, params: { reservation: invalid_params, 'payjp-token' => 'token' }
-          expect(response.status).to eq 200
-        end
-
-        it "エラー文を返す" do
-          post users_reservations_path, params: { reservation: invalid_params, 'payjp-token' => 'token' }
-          expect(response.body).to include "以下のエラーが発生しました："
+          expect(response.status).to eq 500
         end
 
         it "reservationが登録されない" do
           expect do
             post users_reservations_path, params: { reservation: invalid_params, 'payjp-token' => 'token' }
+          end.not_to change(Reservation, :count)
+        end
+      end
+
+      context "api側でエラーが発生した場合" do
+        let(:api_url)           { ENV['PAYJP_CREATE_CHARGE_URL'] }
+        let(:api_request_body)  { { amount: 2000, card: 'token', currency: 'jpy' } }
+
+        before do
+          WebMock.enable!
+          WebMock.stub_request(:post, api_url).with(body: api_request_body).to_raise(Payjp::PayjpError)
+        end
+
+        after do
+          WebMock.disable!
+        end
+
+        it "ステータスコード500を返す" do
+          post users_reservations_path, params: { reservation: params, 'payjp-token' => 'token' }
+          expect(response.status).to eq 500
+        end
+
+        it "reservationが登録されない" do
+          expect do
+            post users_reservations_path, params: { reservation: params, 'payjp-token' => 'token' }
           end.not_to change(Reservation, :count)
         end
       end
@@ -167,41 +187,67 @@ RSpec.describe "Users::Reservations", type: :request do
   end
 
   describe "delete #destroy" do
-    let(:reservation) { create(:reservation, user: user, space: space, charge_id: 'charge_id') }
-    let(:api_url)     { ENV['PAYJP_RETRIEVE_CHARGE_URL'] + reservation.charge_id }
+    let!(:reservation) { create(:reservation, user: user, space: space, charge_id: 'charge_id') }
+    let(:api_url)      { ENV['PAYJP_RETRIEVE_CHARGE_URL'] + reservation.charge_id }
 
     context "ログイン済みの場合" do
       before do
         sign_in user
-        WebMock.enable!
-        WebMock.stub_request(:get, api_url).to_return(status: 200, body: PayjpMocks.get_charge_mock.to_json)
-        allow(ApiPayjp).to receive(:refund).and_return(true)
       end
 
-      after do
-        WebMock.disable!
-      end
+      context "正常に削除できる場合" do
+        before do
+          WebMock.enable!
+          WebMock.stub_request(:get, api_url).to_return(status: 200, body: PayjpMocks.get_charge_mock.to_json)
+          allow(ApiPayjp).to receive(:refund).and_return(true)
+        end
 
-      it "ステータスコード302を返す" do
-        delete users_reservation_path(reservation)
-        expect(response.status).to eq 302
-      end
+        after do
+          WebMock.disable!
+        end
 
-      it "users_reservations_pathにリダイレクトする" do
-        delete users_reservation_path(reservation)
-        expect(response).to redirect_to users_reservations_path
-      end
-
-      it "reservationが削除される" do
-        reservation
-        expect do
+        it "ステータスコード302を返す" do
           delete users_reservation_path(reservation)
-        end.to change(Reservation, :count).by(-1)
+          expect(response.status).to eq 302
+        end
+
+        it "users_reservations_pathにリダイレクトする" do
+          delete users_reservation_path(reservation)
+          expect(response).to redirect_to users_reservations_path
+        end
+
+        it "reservationが削除される" do
+          expect do
+            delete users_reservation_path(reservation)
+          end.to change(Reservation, :count).by(-1)
+        end
+
+        it "フラッシュを返す" do
+          delete users_reservation_path(reservation)
+          expect(flash[:notice]).to eq "予約の削除が完了しました。"
+        end
       end
 
-      it "フラッシュを返す" do
-        delete users_reservation_path(reservation)
-        expect(flash[:notice]).to eq "予約の削除が完了しました。"
+      context "api側でエラーが発生した場合" do
+        before do
+          WebMock.enable!
+          WebMock.stub_request(:get, api_url).to_raise(Payjp::PayjpError)
+        end
+
+        after do
+          WebMock.disable!
+        end
+
+        it "ステータスコード500を返す" do
+          delete users_reservation_path(reservation)
+          expect(response.status).to eq 500
+        end
+
+        it "reservationが削除されない" do
+          expect do
+            delete users_reservation_path(reservation)
+          end.not_to change(Reservation, :count)
+        end
       end
     end
 
