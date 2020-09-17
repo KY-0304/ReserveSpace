4a16b4488677a4ccd768d77e0ba5617f9497d53d
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-09-17 10:03:20 +0900

    累計PV数が多い順にスペースが表示されるようstatic_pages#home, searchを変更。お気に入りした日時の降順で表示するようfavorites#indexを変更

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -2,7 +2,7 @@ class Users::FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @spaces = current_user.favorite_spaces.page(params[:page]).without_count.per(MAX_DISPLAY_SPACE_COUNT)
+    @spaces = current_user.favorite_spaces.order("favorites.created_at DESC").page(params[:page]).without_count.per(MAX_DISPLAY_SPACE_COUNT)
   end
 
   def create
