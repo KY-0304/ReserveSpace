bd1f5222e268f428047ae387c6789550e02ca0b8
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-09-30 17:11:06 +0900

    carrierwave設定変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,10 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage = :fog
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
