30f7a25d14dfc4693a58f3ef9542289b6087b86d
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-10-04 02:04:38 +0900

    image_uploaderの不要コメント削除

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  # space詳細ページ用のサイズ
+
   process resize_to_fill: [1920, 1280]
 
-  # サムネイル用のサイズ
   version :thumb do
     process resize_to_fill: [200, 200]
   end
 
-  # アップロード時のサムネイル
   version :small_thumb, from_version: :thumb do
     process resize_to_fill: [50, 50]
   end
@@ -19,7 +17,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  # テスト環境では独自のパスで保存する設定
   def store_dir
     if Rails.env.test?
       "uploads_#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -28,24 +25,11 @@ class ImageUploader < CarrierWave::Uploader::Base
     end
   end
 
-  # 許容するファイル拡張子
   def extension_whitelist
     %w(jpg jpeg png)
   end
 
-  # ファイルサイズを1KB〜5MBに制限
   def size_range
     1.kilobytes..5.megabytes
   end
-
-  # spaceのimageがnilの時、表示する画像
-  # def default_url(*args)
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 end
